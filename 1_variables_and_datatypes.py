fd02c4c02eac839e59cd3ff5b97e538c6afb4d4f
kmirek1 <karissa.mirek@gmail.com>
2025-10-28 19:28:55 +0000

    edits

--- 1_variables_and_datatypes.py
+++ 1_variables_and_datatypes.py
@@ -30,16 +30,16 @@ print("The sum is:", sum_result)
 # print("Name:", name)
 
 # 2. Numbers: Numeric data (integers or floats)
-age = 23
-print("Age:", age)
+# age = 23
+# print("Age:", age)
 
 # 3. Booleans: True or False values
-hungry = False
-print("Hungry:", hungry)
+# hungry = False
+# print("Hungry:", hungry)
 
 
 # Example combining them all:
-print(f"{name} is {age} years old. Hungry? {hungry}")
+# print(f"{name} is {age} years old. Hungry? {hungry}")
 
 
 # ----------------------------------------
