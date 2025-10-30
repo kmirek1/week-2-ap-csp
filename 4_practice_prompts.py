961d6742d74eb3e01e48eea6c5e2bc9658c6cf9b
kmirek1 <karissa.mirek@gmail.com>
2025-10-30 19:39:37 +0000

    practice

--- 4_practice_prompts.py
+++ 4_practice_prompts.py
@@ -20,7 +20,10 @@ print(500 + 55)
 ##############################################################################################################
 # Find 3 objects around the room and create variables from it,
 # Insert those variables into an f-string sentence(look at slide 22)in repl.it
-
+object1 = "computer"
+object2 = "Lizbeth"
+object3 = "Alexis"
+print("I see " object1 ", " object2 ", and " object3)
 
 # Familiarize yourself with the syntax of the print() function.
 # Print your name.
